af2eb0e932876f5f8d8f003e1cc64e6c7def4cf6
teamlamso <lamso.0ff@gmail.com>
2025-11-25 15:34:39 +0100

    test

--- src/Controller/OuvrierController.php
+++ src/Controller/OuvrierController.php
@@ -12,10 +12,8 @@ final class OuvrierController extends AbstractController
     #[Route('/ouvrier', name: 'app_ouvrier')]
     public function index(): Response
     {
-        $lesOuvriers = $ouvrierRepository->findAll();
-
         return $this->render('ouvrier/index.html.twig', [
-            'ouvriers' => $lesOuvriers,
+            'controller_name' => 'OuvrierController',
         ]);
     }
 }
