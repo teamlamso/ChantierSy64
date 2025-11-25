527d03bdec8bc440285d6779decb78a133279fae
teamlamso <lamso.0ff@gmail.com>
2025-11-25 15:57:58 +0100

    fin partie 3.4 théorique

--- src/Controller/OuvrierController.php
+++ src/Controller/OuvrierController.php
@@ -6,6 +6,7 @@ use App\Repository\OuvrierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\SpecialiteRepository;
 
 #[Route('/ouvrier')]
 final class OuvrierController extends AbstractController
@@ -17,6 +18,14 @@ final class OuvrierController extends AbstractController
 
         return $this->render('ouvrier/index.html.twig', [
             'ouvriers' => $lesOuvriers,
+        ]);
+    }
+
+    #[Route('/showallspecialite', name: 'app_specialite_show_all', methods: ['GET'])]
+    public function showAllSpecialite(SpecialiteRepository $specialiteRepository): Response
+    {
+        return $this->render('ouvrier/show_all_specialite.html.twig', [
+            'specialites' => $specialiteRepository->findAll(),
         ]);
     }
 }
